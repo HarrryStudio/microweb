59868cae184d3eb4659231ed34b4894c95f6115e
lingduanhua <1009291860@qq.com>
2016-01-25 16:35:42 +0800

    添加产品模块的必须文件(没有内容)

---
+++ Public/Home/js/panel/production_list.js
@@ -0,0 +1,36 @@
+$(function(){
+	$(".pattern").on("click",function(){
+		$(".pattern").find("div").removeClass("hr");
+		$(this).find("div").addClass("hr");
+	})
+	var flag = false;
+	var url = $(".production_info a").attr('url');
+	$("#type_all").on("click",function () {
+		$(".type_checkbox").each(function() {
+			this.checked = !flag;
+		})
+		flag = ! flag;
+	}).click();
+	$(".type_checkbox").on('change',function () {
+		if ($(".type_checkbox:checked").length == $(".type_checkbox").length) {
+			$("#type_all")[0].checked = true;
+			flag = true;
+		} else {
+			$("#type_all")[0].checked = false;
+			flag = false;
+		}
+	});
+});
+
+function save() {
+	var save_url = $("#save-url").val();
+	var theme = $(".hr").parent().attr('data-theme');
+	var title = $("#title").val();
+	var selected = $("#type_data").serialize();
+	var column_url = $("#column option:selected").attr('data');
+	var column_id = $("#column").val();
+	var production_url = $("#production_url").val();
+	var option ={title:title, column_id:column_id, column_url:column_url, selected:selected, production_url:production_url};
+	var data = {name:"productionList", theme:theme, source:"production", option:option};
+	window.parent.save(data,$("#status").val());
+};
